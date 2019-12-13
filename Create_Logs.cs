42672f9ffc20d3c52a6d530eeb2bc938901035f6
Aleem AKhter <aleemsiddiqui7585@gmail.com>
2019-12-13 14:57:17 +0500

    fc
    fc

---
+++ Create_Logs.cs
@@ -0,0 +1,53 @@
+﻿using mw_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace mw_api.Change_Logs
+{
+
+    public class Create_Logs : DbContext
+    {
+        public virtual DbSet<ChangeLog> ChangeLogs { get; set; }
+        object GetPrimaryKeyValue(DbEntityEntry entry)
+        {
+            var objectStateEntry = ((IObjectContextAdapter)this).ObjectContext.ObjectStateManager.GetObjectStateEntry(entry.Entity);
+            return objectStateEntry.EntityKey.EntityKeyValues[0].Value;
+        }
+        public override int SaveChanges()
+        {
+            var modifiedEntities = ChangeTracker.Entries()
+                .Where(p => p.State == EntityState.Modified).ToList();
+            var now = DateTime.UtcNow;
+
+            foreach (var change in modifiedEntities)
+            {
+                var entityName = change.Entity.GetType().Name;
+                var primaryKey = GetPrimaryKeyValue(change);
+
+                foreach (var prop in change.OriginalValues.PropertyNames)
+                {
+                    var originalValue = change.OriginalValues[prop].ToString();
+                    var currentValue = change.CurrentValues[prop].ToString();
+                    if (originalValue != currentValue)
+                    {
+                        ChangeLog log = new ChangeLog()
+                        {
+                            EntityName = entityName,
+                            PrimaryKeyValue = primaryKey.ToString(),
+                            PropertyName = prop,
+                            OldValue = originalValue,
+                            NewValue = currentValue,
+                            DateChanged = now
+                        };
+                        ChangeLogs.Add(log);
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
